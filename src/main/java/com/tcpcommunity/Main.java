e1c7c01977e8c943e1567c687e220bfe707b7647
Coolguy3289 <coolguy@thegamingcorner.net>
2017-06-09 18:07:04 -0400

    Oops

--- src/main/java/com/tcpcommunity/Main.java
+++ src/main/java/com/tcpcommunity/Main.java
@@ -12,8 +12,8 @@ public class Main extends ListenerAdapter {
 
     private static JDA api;
     private RoleSetter roleSetter;
-    static long guildID = 275803207345897472L; //Put your guild ID here, MAKE SURE THE L IS AT THE END.
-    static String discordToken = "MzIyNTYyOTE3Nzg4NjgwMTky.DBubJw.YsOATPweBCL4zRzvFfg5MbKl0lQ"; // Put your GuildID in the quotes.
+    static long guildID = 0L; //Put your guild ID here, MAKE SURE THE L IS AT THE END.
+    static String discordToken = ""; // Put your GuildID in the quotes.
 
     public static void main(String[] arguments) throws Exception {
         new Main();
@@ -23,7 +23,7 @@ public class Main extends ListenerAdapter {
         //Add your Discord App Token below in the quotes.
         api = new JDABuilder(AccountType.BOT).setToken(discordToken).buildAsync();
         roleSetter = new RoleSetter();
-        roleSetter.addRoleToList(guildID, "Promo Access");
+        roleSetter.addRoleToList(guildID, "ROLES TO BE ADDED. PUT ROLE IN QUOTES");
 
 
         api.addEventListener(this);
@@ -39,13 +39,13 @@ public class Main extends ListenerAdapter {
         Message message = event.getMessage();
         String content = message.getRawContent();
 
-       if(content.startsWith("+") && content.length() > 1) {
-           String role = content.substring(1);
+       if(content.startsWith("l+") && content.length() > 2) {
+           String role = content.substring(2);
            roleSetter.run(message, role);
        }
 
-       if(content.startsWith("-") && content.length() > 1) {
-           String role = content.substring(1);
+       if(content.startsWith("l-") && content.length() > 2) {
+           String role = content.substring(2);
            roleSetter.run(message, role, true);
        }
 
