04260ba245b3f3d231ddea688fbc569d81b22476
Coolguy3289 <coolguy@thegamingcorner.net>
2017-06-11 03:30:22 -0400

    Changed Default prefix to r+, r- Added rhelp command Added Embed builder for the Help Text.

--- src/main/java/com/tcpcommunity/Main.java
+++ src/main/java/com/tcpcommunity/Main.java
@@ -1,11 +1,11 @@
 package com.tcpcommunity;
 
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.*;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.awt.*;
 
 
 public class Main extends ListenerAdapter {
@@ -39,15 +39,36 @@ public class Main extends ListenerAdapter {
         Message message = event.getMessage();
         String content = message.getRawContent();
 
-       if(content.startsWith("+") && content.length() > 1) {
-           String role = content.substring(1);
+       if(content.startsWith("r+") && content.length() > 2) {
+           String role = content.substring(2);
            roleSetter.run(message, role);
        }
 
-       if(content.startsWith("-") && content.length() > 1) {
-           String role = content.substring(1);
+       if(content.startsWith("r-") && content.length() > 2) {
+           String role = content.substring(2);
            roleSetter.run(message, role, true);
        }
+        if(content.equals("rhelp")) {
+            MessageChannel channel = event.getChannel();
+            Guild guild = message.getGuild();
+            EmbedBuilder rolehelp = new EmbedBuilder();
+
+            rolehelp.setTitle("List of Roles and How to add/remove them!");
+            rolehelp.setDescription("To add a role, type r+ and whichever role you want listed below. To remove roles, type r- and the role you want to remove from the list below.");
+            rolehelp.addField("List of Roles", "``", false);
+            rolehelp.addField("Example Usage", "r+rpg <-- This will add the `RPG` role to you.\nr-rpg <-- This will remove the `RPG` role from you.", true);
+            rolehelp.setColor(new Color(34, 139, 34));
+            channel.sendMessage(rolehelp.build()).queue();
+
+            if(message != null && guild.getMember(message.getJDA().getSelfUser()).hasPermission(message.getTextChannel(), Permission.MESSAGE_MANAGE)) {
+                System.out.println("Deleting message: " + message.getStrippedContent());
+                message.delete().queue();
+            } else {
+                System.out.println("Message already deleted, Ignoring.");
+            }
+
+        }
 
     }
+
 }
